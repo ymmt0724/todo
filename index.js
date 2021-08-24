d55948db8d70482b0a3368f79a21be0a850d69f2
ymmt0724 <89444389+ymmt0724@users.noreply.github.com>
2021-08-24 16:12:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		todoText: '',
+		todos: [        
+		]
+	},
+    methods: {
+		addTodo: function() {
+			var newTodo = this.todoText;
 
+			this.todos.push(
+				{text: newTodo}
+			);
+			this.todoText = '';
+		}, 
+        delTodo: function(index) {
+            this.todos.splice(index, 1);
+          }
+
+
+    }
+	});
